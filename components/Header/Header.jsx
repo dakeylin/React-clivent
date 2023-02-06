372eca0f2f4a44c47f86f25ea31dff255e03dacf
dakeylin <dakeylin@gmail.com>
2023-02-06 23:28:59 +0300

    test

---
+++ components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Title } from './Style'
+
+const Header = () => {
+  return (
+    <div>
+        <Title>Новый текст</Title>        
+    </div>
+  )
+}
+
+export default Header
